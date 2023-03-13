f0c504733c6bbb1a4a3a26d88669f50c8a1758dd
maksimilyan.lamaev.13@inbox.ru <maksimilyan.lamaev.13@inbox.ru>
2023-03-13 10:41:32 +0700

    ьь

--- Form1.cs
+++ Form1.cs
@@ -1,6 +1,9 @@
 ﻿using System.Data;
 using System.Linq;
 using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace WeatherApp
 {
@@ -17,10 +20,25 @@ namespace WeatherApp
 
             WeatherDataGrid.DataSource = _weather.Weathers.ToList();
         }
-
+        
         private void textBox1_TextChanged(object sender, System.EventArgs e)
         {
-            (WeatherDataGrid.DataSource as DataTable).DefaultView.RowFilter = $"CityName LIKE '%{textBox1.Text}%'";
+          //(WeatherDataGrid.DataSource as DataTable).DefaultView.RowFilter = $"CityName LIKE '%{textBox1.Text}%'";
+          WeatherDataGrid.DataSource = _weather.Weathers.Where(u => u.CityName == textBox1.Text).ToList();
+            if (textBox1.Text == "")
+            {
+                WeatherDataGrid.DataSource = _weather.Weathers.ToList();
+            }
+        }
+
+        private void Example()
+        {
+            //List<IntWeather> vs = new List<IntWeather> { new IntWeather("Абакан"), new IntWeather("Черногорск") };
+            ////То, что выводится в DataGridView
+            //WeatherDataGrid.DataSource = vs.ToList();
+
+            ////поиск
+            //WeatherDataGrid.DataSource = vs.Where(u => u.DigitName.Contains(textBox1.Text)).ToList();
         }
     }
 }
---
+++ IntWeather.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    internal class IntWeather
+    {
+        public string DigitName { get; set; }
+
+        public IntWeather(string name)
+        {
+            DigitName = name;
+        }
+    }
+}
